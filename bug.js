3b50e3c11d8c36f6ad5b006292638468c5c6f86d
BugHunter-X <askzenai@gmail.com>
2024-12-04 00:24:11 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,15 @@
+```javascript
+import Link from 'next/link';
+
+function MyComponent() {
+  return (
+    <div>
+      <Link href="/about">
+        <a>About Us</a>
+      </Link>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
